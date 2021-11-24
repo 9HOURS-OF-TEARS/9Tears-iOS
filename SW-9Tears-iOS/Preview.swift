3bfbe40b447d801c0ab16fb0cba2f85d47c608a6
Skyline-23 <bs2740@naver.com>
2021-11-25 07:46:04 +0900

    [UPDATE] update Preview

--- SW-9Tears-iOS/Preview.swift
+++ SW-9Tears-iOS/Preview.swift
@@ -8,12 +8,11 @@
 #if canImport(SwiftUI) && DEBUG
 import SwiftUI
 
-struct ViewControllerPreview: PreviewProvider {
-    static var previews: some View {
-        let reactor = SignInViewReactor()
-        SignInViewController(reactor: reactor).showPreview(.iPhone13Pro)
-    }
-}
+//struct ViewControllerPreview: PreviewProvider {
+//    static var previews: some View {
+//        UIViewController().showPreview(.iPhone13Pro)
+//    }
+//}
 
 //struct ViewPreview: PreviewProvider {
 //    static var previews: some View {
