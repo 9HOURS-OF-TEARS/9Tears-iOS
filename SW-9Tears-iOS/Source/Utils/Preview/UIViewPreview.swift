bb170dd442af812b7ba32f9281072e63537f5693
Skyline-23 <bs2740@naver.com>
2021-11-25 01:44:57 +0900

    [ADD] add preview files

---
+++ SW-9Tears-iOS/Source/Utils/Preview/UIViewPreview.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewPreview.swift
+//  SW-9Tears-iOS
+//
+//  Created by 김부성 on 2021/11/25.
+//
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+extension UIView {
+    private struct Preview: UIViewRepresentable {
+
+        let view: UIView
+
+        func makeUIView(context: Context) -> UIView {
+            return view
+        }
+
+        func updateUIView(_ uiView: UIView, context: Context) {
+        }
+    }
+
+    func showPreview(width: CGFloat, height: CGFloat) -> some View {
+        Preview(view: self).previewLayout(.fixed(width: width, height: height))
+    }
+}
+#endif
+
