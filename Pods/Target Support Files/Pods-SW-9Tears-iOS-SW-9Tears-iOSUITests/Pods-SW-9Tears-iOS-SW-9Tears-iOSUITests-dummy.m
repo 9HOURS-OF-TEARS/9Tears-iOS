031a0178ccda2603e54ff452bf55f932db5f37c8
Skyline-23 <bs2740@naver.com>
2021-11-24 23:55:09 +0900

    [DELETE] remove Pods

--- Pods/Target Support Files/Pods-SW-9Tears-iOS-SW-9Tears-iOSUITests/Pods-SW-9Tears-iOS-SW-9Tears-iOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SW_9Tears_iOS_SW_9Tears_iOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SW_9Tears_iOS_SW_9Tears_iOSUITests
-@end
