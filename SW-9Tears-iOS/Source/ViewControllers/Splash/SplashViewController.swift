151a83a919c73a85372e0336e125482530b045a2
Skyline-23 <bs2740@naver.com>
2021-11-25 12:07:32 +0900

    [UPDATE] update SplashView

--- SW-9Tears-iOS/Source/ViewControllers/Splash/SplashViewController.swift
+++ SW-9Tears-iOS/Source/ViewControllers/Splash/SplashViewController.swift
@@ -15,11 +15,12 @@ final class SplashViewController: BaseViewController, View {
     
     // MARK: - Constants
     fileprivate struct Metric {
-        
+        static let imageSize = 135.f
     }
     
     fileprivate struct Font {
-        
+        static let titleFont = UIFont(name: "BlackHanSans-Regular", size: 60)
+        static let subFont = UIFont.systemFont(ofSize: 13, weight: .regular)
     }
     
     fileprivate struct Style {
@@ -29,6 +30,22 @@ final class SplashViewController: BaseViewController, View {
     // MARK: - Properties
     
     // MARK: - UI
+    let logo = UIImageView().then {
+        $0.image = UIImage.init(named: "SplashLogo")
+        $0.tintColor = .white
+    }
+    
+    let titleLabel = UILabel().then {
+        $0.text = "바랭"
+        $0.font = Font.titleFont
+        $0.textColor = .white
+    }
+    
+    let subLabel = UILabel().then {
+        $0.text = "9시간의 눈물"
+        $0.font = Font.subFont
+        $0.textColor = .white
+    }
     
     // MARK: - Inintializing
     init(reactor: Reactor) {
@@ -50,12 +67,30 @@ final class SplashViewController: BaseViewController, View {
     override func setupLayout() {
         super.setupLayout()
         
-        self.view.backgroundColor = .black
+        self.view.backgroundColor = .tintColor
+        
+        self.view.addSubview(self.logo)
+        self.view.addSubview(self.titleLabel)
+        self.view.addSubview(self.subLabel)
     }
     
     override func setupConstraints() {
         super.setupConstraints()
         
+        self.logo.snp.makeConstraints {
+            $0.height.width.equalTo(Metric.imageSize)
+            $0.center.equalToSuperview()
+        }
+        
+        self.titleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.logo.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+        }
+        
+        self.subLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSafeArea(self.view)
+        }
     }
     
     // MARK: - Configuring
