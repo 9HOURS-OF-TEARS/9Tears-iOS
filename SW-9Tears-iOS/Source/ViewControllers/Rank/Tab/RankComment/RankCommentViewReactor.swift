ce8a9a5e755e234d42994adb891958c5d802ae9a
jangseoyoung <jangseoyoung0402@gmail.com>
2021-11-25 13:41:42 +0900

    [CREATE] Reactor

---
+++ SW-9Tears-iOS/Source/ViewControllers/Rank/Tab/RankComment/RankCommentViewReactor.swift
@@ -0,0 +1,43 @@
+//
+//  RankCommentViewReactor.swift
+//  SW-9Tears-iOS
+//
+//  Created by 장서영 on 2021/11/25.
+//
+
+import Foundation
+
+import ReactorKit
+import RxRelay
+import RxFlow
+
+final class RankCommentViewReactor: Reactor, Stepper {
+    
+    var steps: PublishRelay<Step> = PublishRelay<Step>()
+    
+    let initialState: State
+    
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    init() {
+        self.initialState = State()
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        
+    }
+}
