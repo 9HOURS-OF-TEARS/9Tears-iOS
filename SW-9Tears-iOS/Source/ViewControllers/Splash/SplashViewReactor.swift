24980c301ff4eb1f552eea86ce2329e565e4dc5c
Skyline-23 <bs2740@naver.com>
2021-11-25 11:25:48 +0900

    [ADD] add SplashViewReactor

---
+++ SW-9Tears-iOS/Source/ViewControllers/Splash/SplashViewReactor.swift
@@ -0,0 +1,44 @@
+//
+//  SplashViewReactor.swift
+//  SW-9Tears-iOS
+//
+//  Created by 김부성 on 2021/11/25.
+//
+
+import Foundation
+
+import ReactorKit
+import RxRelay
+import RxFlow
+
+final class SplashViewReactor: Reactor, Stepper {
+    
+    var steps: PublishRelay<Step> = PublishRelay<Step>()
+    
+    let initialState: State
+    
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    init() {
+        self.initialState = State()
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        
+    }
+}
+
